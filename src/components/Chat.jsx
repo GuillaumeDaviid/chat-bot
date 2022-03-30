35ab9a53c46e34abd54adacd57288ab3eaca86ff
Guillaume David <guillaumedaavidpro@gmail.com>
2022-03-30 16:04:29 +0200

    html: create form

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,13 @@
 function Chat() {
     return(
-        <div></div>
+        <div>
+            <h1>Chat Bot</h1>
+            <div className="chat_bot" id="chat_bot"></div>
+            <form >
+                <input></input>
+                <button>Envoyer</button>
+            </form>
+        </div>
     )
 }
 
