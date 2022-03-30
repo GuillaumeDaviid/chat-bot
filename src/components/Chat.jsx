3374b2fc1ca0c5c6f55c3c323d3380fd724311f8
Guillaume David <guillaumedaavidpro@gmail.com>
2022-03-30 16:31:24 +0200

    css: style Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,7 +4,12 @@ function Chat() {
     return(
         <div>
             <h1>Chat Bot</h1>
-            <div className="chat_bot" id="chat_bot"></div>
+            <div className="chat_bot" id="chat_bot">
+                <div className='chat_bot_content-msg' id='chat_bot_content-msg'>
+                <h2 className='chat_bot-msg'>Bot :</h2>
+                <p className='chat_bot-msg'>Bonjour, Je suis un bot codé en React.js</p>
+                </div>
+            </div>
             <form className='form'>
                 <input className='msg' id="msg" placeholder='Message'></input>
                 <button className='btn'>Envoyer</button>
