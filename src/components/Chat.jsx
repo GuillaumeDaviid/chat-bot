befeb2b7db62e935937716c9b5d11a34db40e95c
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-07 15:44:35 +0200

    new msg

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -16,10 +16,15 @@ function Chat() {
 
     function msgBot(newMsg) {
         if (newMsg.toLowerCase() === "bonjour"){
-            return (<p className='chat_bot-msg'>Bonjour</p>)
+            return (<p className='chat_bot-msg'>Bonjour !</p>)
         }
         else if (newMsg.toLowerCase() === "!help"){
-            return (<p className='chat_bot-msg'>Voici une liste de commande possible</p>)
+            return (<p className='chat_bot-msg'>Voici une liste de commande possible : 
+            <br/> - !help 
+            <br/> - !freelance</p>)
+        }
+        else if (newMsg.toLowerCase() === "!freelance"){
+            return (<p className='chat_bot-msg'>Si vous souhaitez prendre contact avec un freelance, vous pouvez le faire ici: <a href="https://www.guillaumedavid.com">guillaumedavid.com</a></p>)
         }
         else {
             return (<p className='chat_bot-msg'>Je n'est pas compris ton dernier message</p>)
