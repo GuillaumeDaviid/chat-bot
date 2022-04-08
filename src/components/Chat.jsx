0127eb9ac43b3f09b5223a800beadf8fbc32597b
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-08 14:41:46 +0200

    Messages component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './Chat.css'
+import { msgBot } from './Messages.jsx';
 
 function Chat() {
 
@@ -12,23 +13,6 @@ function Chat() {
         setCountMsg(countMsg + 1);
         setNewMsg(newMsg.concat(msg))
         setMsg('');
-    }
-
-    function msgBot(newMsg) {
-        if (newMsg.toLowerCase() === "bonjour"){
-            return (<p className='chat_bot-msg'>Bonjour !</p>)
-        }
-        else if (newMsg.toLowerCase() === "!help"){
-            return (<p className='chat_bot-msg'>Voici une liste de commande possible : 
-            <br/> - !help 
-            <br/> - !freelance</p>)
-        }
-        else if (newMsg.toLowerCase() === "!freelance"){
-            return (<p className='chat_bot-msg'>Si vous souhaitez prendre contact avec un freelance, vous pouvez le faire ici: <a href="https://www.guillaumedavid.com" rel="noreferrer" target="_blank">guillaumedavid.com</a></p>)
-        }
-        else {
-            return (<p className='chat_bot-msg'>Je n'est pas compris ton dernier message</p>)
-        }
     }
 
     function handleChange(e){
