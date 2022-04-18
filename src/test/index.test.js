bc4a5432fce7007b9f8a489d7ac9a6a44a3db3d0
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-18 13:52:02 +0200

    first test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import Chat from "../components/Chat.jsx";
 import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved, act } from '@testing-library/react';
+
+describe('Chat Bot', () => {
+    test('Should render without crashing', async () => {
+        render(
+            <Chat />
+        )
+    })
+
+
+})
