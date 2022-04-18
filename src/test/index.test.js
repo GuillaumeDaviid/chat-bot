9d42f5bdd426c498c63a70a9a1f4f1b109e9ea90
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-18 14:12:27 +0200

    jsx: new test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Chat from "../components/Chat.jsx";
-import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved, act } from '@testing-library/react';
+import { msgBot } from '../components/Messages'
+import { render, screen } from '@testing-library/react';
 
 describe('Chat Bot', () => {
     test('Should render without crashing', async () => {
@@ -9,5 +10,21 @@ describe('Chat Bot', () => {
         )
     })
 
+    test('should display chat', async () => {
+        render(
+            <Chat />
+        )
+        const button = screen.getByRole('button')
+        expect(button.textContent).toBe("Envoyer")
+    })
 
+    test('should display default message', async () => {
+        const myMessage = msgBot("random msg")
+        expect(myMessage).toStrictEqual(<p className='chat_bot-msg'>Je n'est pas compris ton dernier message</p>)
+    })
+
+    test('should to transform the message to lower case', async () => {
+        const myMessage = msgBot("BonJour")
+        expect(myMessage).toStrictEqual(<p className='chat_bot-msg'>Bonjour !</p>)
+    })
 })
